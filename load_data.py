97e7279d9d50d2de43c832bf0691e72626b3da61
SeriousProgramer <113993748+SeriousProgramer@users.noreply.github.com>
2023-11-23 13:30:51 +0100

    Update load_data.py

--- load_data.py
+++ load_data.py
@@ -1,14 +1,23 @@
 import pandas as pd
 import numpy as np
-#replace <path on you computer to file, e.g. C:\....> with the path to the file 
-df = pd.read_csv('C:\\Users\\20221498\\Desktop\\Visualization\\all_data.csv',delimiter=';', on_bad_lines='skip')
+
+# replace <path on you computer to file, e.g. C:\....> with the path to the file
+df = pd.read_csv(
+    "C:\\Users\\20221498\\Desktop\\Visualization\\all_data.csv",
+    delimiter=";",
+    on_bad_lines="skip",
+)
 print(df)
 
+
 def cell_value():
-    return df.iat[0,0]
+    return df.iat[0, 0]
+
 
 print(cell_value())
 print(df.columns)
+
+
 def count_freq_and_describe():
     for column in df.columns:
         print(df[column].value_counts())
@@ -17,9 +26,7 @@ def count_freq_and_describe():
 
 def findMissing():
     for column in df.columns:
-     num_empty = (df[column] == '').sum()
-     print(num_empty, "empty cells in column", column)
-     print(df[column].isna().sum())
+        print(df[column].isna().sum())
+
 
 findMissing()
-
