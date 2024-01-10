6a728e614a3f72a2338c5e305b27962a74902f02
SeriousProgramer <113993748+SeriousProgramer@users.noreply.github.com>
2024-01-10 10:18:58 +0100

    Tried some stuff with dash

--- load_data.py
+++ load_data.py
@@ -1,65 +1,30 @@
 import pandas as pd
 import numpy as np
+import dash
+from dash import dcc, html
+import plotly.express as px
 
 # replace <path on you computer to file, e.g. C:\....> with the path to the file
 df = pd.read_csv(
-    "C:\\Users\\20221498\\Desktop\\Visualization\\all_data.csv",
+    "C:\\Users\\20221498\\Desktop\\Visualization\\cleaned_data.csv",
     delimiter=";",
     on_bad_lines="skip",
 )
-print(df)
+dfs = df.head(50)
+
+fig = px.scatter(dfs, x='Month', y='Num_of_Delayed_Payment', title='Scatter plot of Number of Delayed Payments over Month')
+
+# Create a Dash application
+app = dash.Dash(__name__)
+
+# Define the layout
+app.layout = html.Div([
+    dcc.Graph(figure=fig)
+])
+
+# Run the Dash app
+if __name__ == '__main__':
+    app.run_server(debug=True)
 
 
-def cell_value():
-    return df.iat[0, 0]
 
-
-print(cell_value())
-print(df.columns)
-
-
-def count_freq_and_describe():
-    for column in df.columns:
-        print(df[column].value_counts())
-        print(df[column].describe())
-
-
-def findMissing():
-    for column in df.columns:
-        print(df[column].isna().sum() + column)
-
-def is_valid_int(s):
-    try:
-        int(s)
-        return True
-    except ValueError:
-        return False
-
-def wrongAge():
-    for i in df['Age']:
-        if not is_valid_int(i) or int(i) < 10 or int(i) > 100:
-            print(i)
-# wrongAge()
-
-def badName():
-    for i in df['Name']:
-        if not i.isalpha():
-            print(i)
-            
-def count_consecutive_nans():
-    # Create a boolean series where True represents NaN values
-    is_nan = df['Name'].isna()
-
-    # Initialize count of four consecutive NaNs
-    count = 0
-
-    # Iterate over the boolean series with a sliding window of size 4
-    for i in range(len(is_nan) - 3):
-        # If all four values in the window are True (NaN), increment the count
-        if is_nan[i] and is_nan[i+1] and is_nan[i+2] and is_nan[i+3]:
-            count += 1
-
-    print(f'Number of times four consecutive NaN values occur: {count}')
-
-# count_consecutive_nans()
-print(df['Credit_Score'].isna().sum())
