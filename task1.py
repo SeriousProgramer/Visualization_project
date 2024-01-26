acdf930cca0c6bd2d134a0c513b44bed1603241a
Yash-Dandriyal <yash.sama007@gmail.com>
2024-01-26 22:56:50 +0100

    update task 1

--- task1.py
+++ task1.py
@@ -4,4 +4,5 @@ import plotly.graph_objs as go
 class Task1(Task):
     def get_plot(self):
         # Example plot for Task 1
+        #return 
         return go.Figure(data=[go.Bar(x=[1, 2, 3], y=[1, 3, 2])])
